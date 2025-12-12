c7f83f5867a19c9f9d9a05af9229c657942de7e7
akhiakter25556 <akhiakterherpower80@gmail.com>
2025-12-12 18:05:19 +0600

    up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Warning limit increase
+    chunkSizeWarningLimit: 1000, // KB
+
+    // Manual chunks for better code splitting
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'lodash']
+        }
+      }
+    }
+  }
 })
